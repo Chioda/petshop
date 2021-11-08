22f0ee80efee17bb7a07d5925990d7272edbdf03
Thiago Vieira Chioda <thiagochioda@hotmail.com>
2021-11-07 21:17:57 -0300

    Tratando exceções e referencias ciclicas

--- src/main/java/com/thiago/petshop/repositories/ProdutoRepository.java
+++ src/main/java/com/thiago/petshop/repositories/ProdutoRepository.java
@@ -2,7 +2,6 @@ package com.thiago.petshop.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.yaml.snakeyaml.events.Event.ID;
 
 import com.thiago.petshop.domain.Produto;
 
