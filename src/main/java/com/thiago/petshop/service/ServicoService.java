90b28990dc204b32bb6a8ca5c2ab43c6a5c24f09
Thiago Vieira Chioda <thiagochioda@hotmail.com>
2021-11-13 21:06:55 -0300

    POST, PUT, e DELETE de pessoa e serviço + findAll

--- src/main/java/com/thiago/petshop/service/ServicoService.java
+++ src/main/java/com/thiago/petshop/service/ServicoService.java
@@ -3,10 +3,12 @@ package com.thiago.petshop.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.thiago.petshop.domain.Servico;
 import com.thiago.petshop.repositories.ServicoRepository;
+import com.thiago.petshop.service.exceptions.DataIntegrityException;
 import com.thiago.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 @Service
@@ -18,5 +20,24 @@ public class ServicoService {
 	public Servico find(Integer id) {
 		Optional<Servico> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjetoNaoEncontradoException("Objeto não encontrado. ID: " + id + ", Tipo: " + Servico.class.getName()));
+	}
+	
+	public Servico insert(Servico obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Servico update(Servico obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}	
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Servico possui produtos, não é possível deletar!");
+		}
 	}
 }
